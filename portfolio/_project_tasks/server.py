2d632160af2c7bcc31a4de10bfb35578d2f8b2ad
who is juarez? <133733073+whoisjuarez@users.noreply.github.com>
2024-02-12 11:50:51 -0500

    Docstrings added

--- portfolio/_project_tasks/server.py
+++ portfolio/_project_tasks/server.py
@@ -1,3 +1,8 @@
+"""
+Module of a HTTP server for managing tasks.
+
+The server supports GET, POST, and DELETE requests.
+"""
 from http.server import HTTPServer, BaseHTTPRequestHandler
 from tasks import TaskStore
 from form_urlencoded import get_POST_data
@@ -5,37 +10,68 @@ from form_urlencoded import get_POST_data
 task_store = TaskStore()
 
 class TaskHandler(BaseHTTPRequestHandler):
-   def send_msg(self, status_code, message):
-      self.send_response(status_code)
-      self.end_headers()
-      self.wfile.write((message + "\n").encode())
+    """
+    A request handler for a HTTP server that manages tasks.
 
-   def do_GET(self):
-      if self.path == "/":
-         self.send_msg(200, "Showing All Tasks\n" + str(task_store.tasks) + "\n")
-      elif self.path.startswith("/task/"):
-         task_id = int(self.path.split('/')[-1])
-         task = task_store.get_task(task_id)
-         if task:
-            self.send_msg(200, "Showing Task: " + str(task) + "\n")
-         else:
-            self.send_msg(404, "Task Id " + str(task_id) + " Not Found\n")
+    This handler supports GET, POST, and DELETE requests.
+    """
+    def send_msg(self, status_code, message):
+        """
+        Send a HTTP response with the given status code and message.
 
-   def do_POST(self):
-      if self.path == "/task":
-         data = get_POST_data(self)
-         name = data["name"]
-         due_date = data["due_date"] if "due_date" in data else None
-         task_to_create = task_store.create_task(name, due_date)
-         self.send_msg(201, "Task Created\n" + str(task_to_create) + "\n")
+        Args:
+            status_code (int): The HTTP status code for the response.
+            message (str): The message to include in the response body.
+        """
+        self.send_response(status_code)
+        self.end_headers()
+        self.wfile.write((message + "\n").encode())
 
-   def do_DELETE(self):
-      task_id = int(self.path.split('/')[-1])
-      if self.path.startswith("/task/"):
-         task_store.delete_task(task_id)
-         self.send_msg(200, "Task Id " + str(task_id) + " Deleted")
-      else:
-         self.send_msg(404, "Task "+ str(task_id) + " Not Found")
+    def do_GET(self):
+        """
+        Handle a GET request by responding with the list of tasks or a specific task.
+
+        If the path is '/', respond with the list of tasks.
+        If the path starts with '/task/', respond with the task with the given ID.
+        """
+        if self.path == "/":
+            self.send_msg(200, "Showing All Tasks\n" + str(task_store.tasks) + "\n")
+        elif self.path.startswith("/task/"):
+            task_id = int(self.path.split('/')[-1])
+            task = task_store.get_task(task_id)
+            if task:
+                self.send_msg(200, "Showing Task: " + str(task) + "\n")
+            else:
+                self.send_msg(404, "Task Id " + str(task_id) + " Not Found\n")
+
+    def do_POST(self):
+        """
+        Handle a POST request by creating a new task.
+
+        If the path is '/task', create a new task with the given name and due date.
+        """
+        if self.path == "/task":
+            data = get_POST_data(self)
+            name = data["name"]
+            due_date = data["due_date"] if "due_date" in data else None
+            task_to_create = task_store.create_task(name, due_date)
+            self.send_msg(201, "Task Created\n" + str(task_to_create) + "\n")
+
+    def do_DELETE(self):
+        """
+        Handle a DELETE request by deleting a task.
+
+        If the path starts with '/task/', delete the task with the given ID.
+        """
+        if self.path == "/":
+            self.send_msg(405, "Method Not Allowed")
+            return
+        task_id = int(self.path.split('/')[-1])
+        if self.path.startswith("/task/"):
+            task_store.delete_task(task_id)
+            self.send_msg(200, "Task Id " + str(task_id) + " Deleted")
+        else:
+            self.send_msg(404, "Task "+ str(task_id) + " Not Found")
 
 address = ("localhost", 8080)
 server = HTTPServer(address, TaskHandler)
--- portfolio/_project_tasks/tasks.py
+++ portfolio/_project_tasks/tasks.py
@@ -1,20 +1,59 @@
+"""
+Tasks module for managing tasks.
+"""
+
 class TaskStore:
-   def __init__(self):
-      self.tasks = []
-      self.counter = 1
+    """
+    A class for managing tasks.
 
-   def create_task(self, name, due_date=None):
-      new_task = {"id": self.counter, "name": name, "due_date": due_date}
-      self.tasks.append(new_task)
-      self.counter += 1
-      return new_task
+    This class stores tasks in a list of dictionaries.
+    Each dictionary has an 'id', 'name', and 'due_date' field.
+    """
+    def __init__(self):
+        """
+        Initialize the TaskStore class.
+        """
+        self.tasks = []
+        self.counter = 1
 
-   def get_task(self, id):
-      for task in self.tasks:
-         if task["id"] == id:
-            return task
-      
-   def delete_task(self, id):
-      task_to_delete = self.get_task(id)
-      self.tasks.remove(task_to_delete)
-      print(f"Task {id} deleted")
+    def create_task(self, name, due_date=None):
+        """
+        Create a new task.
+
+        Args:
+            name (str): The name of the task.
+            due_date (str): The due date of the task.
+
+        Returns:
+            dictionary: The newly created task.
+        """
+        new_task = {"id": self.counter, "name": name, "due_date": due_date}
+        self.tasks.append(new_task)
+        self.counter += 1
+        return new_task
+
+    def get_task(self, task_id):
+        """
+        Get a task by its ID.
+
+        Args:
+            task_id (int): The ID of the task.
+
+        Returns:
+            dictionary: The task with the given ID, or None if not found.
+        """
+        for task in self.tasks:
+            if task["task_id"] == task_id:
+                return task
+        return None
+
+    def delete_task(self, task_id):
+        """
+        Delete a task by its ID.
+
+        Args:
+            task_id (int): The ID of the task to delete.
+        """
+        task_to_delete = self.get_task(task_id)
+        self.tasks.remove(task_to_delete)
+        print(f"Task {task_id} deleted")
